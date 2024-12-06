39584c00cac36c3991bec8a5d260bba2eec7ba16
Mark Austin <mark@itcambridge.co.uk>
2024-12-06 10:24:47 +0000

    Fix build error: Add missing motion import

--- src/app/industries/banking/page.tsx
+++ src/app/industries/banking/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import ServicePageLayout from '@/components/Layout/ServicePageLayout'
 import IntelligentAnalysisHub from '@/components/Effects/IntelligentAnalysisHub'
 
